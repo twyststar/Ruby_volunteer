e102fa367de92393fbba4b4452ade788813d2436
Starlynne <lewis.start0905@gmail.com>
2017-05-05 12:07:50 -0700

    Add edit and delete to project and volunteer pages

--- app.rb
+++ app.rb
@@ -28,6 +28,22 @@ get('/single_project/:id') do
   erb(:single_project)
 end
 
+patch('/single_project/:id') do
+  name = params.fetch("name")
+  description = params.fetch('description')
+  @project = Project.find(params.fetch("id").to_i())
+  @project.update({:name => name, :description => description})
+  @volunteers = Volunteer.all()
+  erb(:single_project)
+end
+delete('/single_project/:id') do
+  @project = Project.find(params.fetch("id").to_i())
+  @project.delete()
+  @volunteers = Volunteer.all()
+  @projects = Project.all()
+  erb(:index)
+end
+
 post('/volunteers') do
   name = params.fetch("name")
   skill = params.fetch('skill')
@@ -42,4 +58,22 @@ end
 get('/single_volunteer/:id') do
   @volunteer = Volunteer.find(params.fetch('id').to_i())
   erb(:single_volunteer)
+end
+
+patch('/single_volunteer/:id') do
+  name = params.fetch("name")
+  skill = params.fetch('skill')
+  @volunteer = Volunteer.find(params.fetch("id").to_i())
+  @volunteer.update({:name => name, :skill => skill})
+  @volunteers = Volunteer.all()
+  erb(:single_volunteer)
+end
+
+delete('/single_volunteer/:id') do
+  @volunteer = Volunteer.find(params.fetch("id").to_i())
+  @project_id = @volunteer.project_id().to_i()
+  @volunteer.delete()
+  @volunteers = Volunteer.all()
+  @project = Project.find(@project_id)
+  erb(:single_project)
 end
